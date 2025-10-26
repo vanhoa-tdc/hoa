3376cd246ce1e6224977b7a65ae79990643f32e0
vanhoa-tdc <hoa96610@gmail.com>
2025-10-26 13:51:38 +0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,10 +1,10 @@
 <?php
 $configDB = array();
-$configDB["host"] 		= "localhost";
+$configDB["host"] 		= "hoa.mysql.database.azure.com";
 $configDB["database"]	= "computer_store";
 $configDB["username"] 	= "hoa";
 $configDB["password"] 	= "tdc@12345";
-define("HOST", "localhost");
+define("HOST", "hoa.mysql.database.azure.com");
 define("DB_NAME", "computer_store");
 define("DB_USER", "hoa");
 define("DB_PASS", "tdc@12345");
